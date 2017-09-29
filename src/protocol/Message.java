e79b9392611cfd5a5ace7ae4cfc7a0f66008fd31
Maxwell Littlejohn <maxwelllittlejohn@gmail.com>
2017-09-29 17:57:57 +1000

    protocol added

---
+++ src/protocol/Message.java
@@ -0,0 +1,44 @@
+package protocol;
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Max
+ */
+public class Message implements Serializable{
+
+    private String message;
+    private int type;
+    private Object object;
+    
+    public static final int MESSAGE = 1; 
+    public static final int TURN = 2; 
+    public static final int PLAY = 3; 
+    public static final int RULES = 4;
+    public static final int EXIT = 5;
+    
+    public Message(String message, int type, Object object) {
+        this.message = message;
+        this.type = type;
+        this.object = object;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public int getType() {
+        return type;
+    }
+    
+    public Object getObject() {
+        return object;
+    }
+}
